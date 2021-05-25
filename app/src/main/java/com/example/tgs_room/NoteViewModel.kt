eb225a866df5f174bbf75ec8f3599dd5614754e8
Afi <afifahmardhiyahr@gmail.com>
2021-05-26 01:03:34 +0800

    Buat Note View Model Class

---
+++ app/src/main/java/com/example/tgs_room/NoteViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.tgs_room
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.tgs_room.db.Note
+import com.example.tgs_room.db.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var noteRepository = NoteRepository(application)
+    private var notes: LiveData<List<Note>>? = noteRepository.getNotes()
+
+    fun insertNote(note: Note) {
+        noteRepository.insert(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>>? {
+        return notes
+    }
+
+    fun deleteNote(note: Note) {
+        noteRepository.delete(note)
+    }
+
+    fun updateNote(note: Note) {
+        noteRepository.update(note)
+    }
+
+}
